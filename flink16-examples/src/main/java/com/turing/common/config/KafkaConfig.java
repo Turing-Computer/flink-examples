2607aad366136499065aa47a529947c9dee0d8b2
lj.michale <economic_luojie@outlook.com>
2023-03-10 15:38:23 +0800

    [feat][flink]基于新版Kafka Connector的工具类

---
+++ flink16-examples/src/main/java/com/turing/common/config/KafkaConfig.java
@@ -0,0 +1,12 @@
+package com.turing.common.config;
+/**
+ * @descri
+ *
+ * @author lj.michale
+ * @date 2023-03-10
+ */
+public interface KafkaConfig {
+
+    String bootstrapServers = "192.168.10.102:9092";
+
+}
