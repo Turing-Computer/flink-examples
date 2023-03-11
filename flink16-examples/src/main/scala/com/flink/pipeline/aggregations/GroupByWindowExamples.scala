355abec006b5b9cf906673a952109e678040074a
lj.michale <economic_luojie@outlook.com>
2023-03-11 16:19:07 +0800

    [feat][flink][faker]Window Top-N

--- flink16-examples/src/main/scala/com/flink/pipeline/aggregations/GroupByWindowExamples.scala
+++ flink16-examples/src/main/scala/com/flink/pipeline/aggregations/GroupByWindowExamples.scala
@@ -56,8 +56,47 @@ object GroupByWindowExamples {
         |GROUP BY
         |  TUMBLE(log_time, INTERVAL '1' MINUTE);
         |""".stripMargin
-    val executeSqlArrary = executeSql.split(";")
 
+    val executeSql2 =
+      """
+        |CREATE TABLE orders (
+        |    bidtime TIMESTAMP(3),
+        |    price DOUBLE,
+        |    item STRING,
+        |    supplier STRING,
+        |    WATERMARK FOR bidtime AS bidtime - INTERVAL '5' SECONDS
+        |) WITH (
+        |  'connector' = 'faker',
+        |  'fields.bidtime.expression' = '#{date.past ''30'',''SECONDS''}',
+        |  'fields.price.expression' = '#{Number.randomDouble ''2'',''1'',''150''}',
+        |  'fields.item.expression' = '#{Commerce.productName}',
+        |  'fields.supplier.expression' = '#{regexify ''(Alice|Bob|Carol|Alex|Joe|James|Jane|Jack)''}',
+        |  'rows-per-second' = '100'
+        |);
+        |CREATE TABLE sink_table (
+        |    window_start TIMESTAMP(3),
+        |    window_end TIMESTAMP(3),
+        |	   supplier STRING,
+        |	   price DOUBLE,
+        |    cnt BIGINT,
+        |    rownum BIGINT
+        |) WITH (
+        |  'connector' = 'print'
+        |);
+        |INSERT INTO sink_table
+        |SELECT *
+        |    FROM (
+        |        SELECT *, ROW_NUMBER() OVER (PARTITION BY window_start, window_end ORDER BY price DESC) as rownum
+        |        FROM (
+        |            SELECT window_start, window_end, supplier, SUM(price) as price, COUNT(*) as cnt
+        |            FROM TABLE(
+        |                TUMBLE(TABLE orders, DESCRIPTOR(bidtime), INTERVAL '10' MINUTES))
+        |            GROUP BY window_start, window_end, supplier
+        |        )
+        |    ) WHERE rownum <= 3;
+        |""".stripMargin
+
+    val executeSqlArrary = executeSql2.split(";")
     /**
      * 算子 {@link org.apache.flink.streaming.api.operators.KeyedProcessOperator}
      * --  {@link org.apache.flink.table.runtime.operators.deduplicate.ProcTimeDeduplicateKeepFirstRowFunction}
