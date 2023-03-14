8590221b231fac952a0d7bb97ed5a17a7379fe88
lj.michale <economic_luojie@outlook.com>
2023-03-14 10:07:52 +0800

    [feat][flinksql]FlinkTableStreamPipelineByStreamTableEnvironmentDemo

---
+++ flink16-examples/src/main/scala/com/flink/pipeline/example7/FlinkTableBatchPipelineByStreamTableEnvironmentDemo.scala
@@ -0,0 +1,30 @@
+package com.flink.pipeline.example7
+
+import com.turing.common.FlinkEnvUtils
+import org.apache.flink.api.common.RuntimeExecutionMode
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment
+import org.slf4j.LoggerFactory
+
+object FlinkTableBatchPipelineByStreamTableEnvironmentDemo {
+
+  private val logger = LoggerFactory.getLogger(this.getClass.getName)
+
+  def main(args: Array[String]): Unit = {
+
+    val fileName = "application-dev.properties"
+    val inputStream = this.getClass.getClassLoader.getResourceAsStream(fileName)
+    val parameterTool = ParameterTool.fromPropertiesFile(inputStream)
+    val parallelisNum = Integer.valueOf(parameterTool.get("flink.pipeline.parallelism", "1"))
+    val checkpointPath = parameterTool.get("flink.pipeline.checkpoint.url")
+    logger.info("load global property file:{}", fileName)
+
+    val flinkEnv:StreamExecutionEnvironment = FlinkEnvUtils.getStreamTableEnv(args).getStreamExecutionEnvironment
+    flinkEnv.setRuntimeMode(RuntimeExecutionMode.BATCH)
+    val tEnv = StreamTableEnvironment.create(flinkEnv)
+
+    flinkEnv.execute(s"${this.getClass.getName}")
+  }
+
+}
